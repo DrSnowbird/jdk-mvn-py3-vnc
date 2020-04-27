4cc650a9aeee7499ae10166a41b25696f147b40a
openkbs <rsheu@openkbs.org>
2020-04-27 13:38:58 -0400

    google-chrome.sh remove openbox since it is causing build issue and not being used

--- src/ubuntu/install/google-chrome.sh
+++ src/ubuntu/install/google-chrome.sh
@@ -9,6 +9,9 @@ cd /tmp
 sudo apt-get update -y 
 sudo apt-get upgrade -y 
 
+# -- patch (work-around) fix to prevent 
+#echo "Set disable_coredump false" | sudo tee -a /etc/sudo.conf
+
 # (Ubuntu 16.04 only)
 # sudo apt-get install -y libindicator3-7 indicator-application libnss3-nssdb libnss3 libnspr4 libappindicator3-1 fonts-liberation xdg-utils
 sudo apt-get install -y libindicator3-7 indicator-application libnss3-dev libnss3-tools libnss3 libnspr4  libappindicator3-1 fonts-liberation xdg-utils libgbm1 libu2f-udev
@@ -24,7 +27,9 @@ rm google-chrome-stable_current_amd64.deb
 
 echo "CHROMIUM_FLAGS='--no-sandbox --no-gpu --start-maximized --user-data-dir'" > $HOME/.chromium-browser.init
 
-sudo apt-get install -y xvfb feh openbox obmenu tint2 xfce4-panel xfce4-notifyd xfce4-whiskermenu-plugin compton conky-all
+#sudo apt-get install -y xvfb feh openbox obmenu tint2 xfce4-panel xfce4-notifyd xfce4-whiskermenu-plugin compton conky-all
+
+sudo apt-get install -y xvfb feh openbox tint2 xfce4-panel xfce4-notifyd xfce4-whiskermenu-plugin compton conky-all
 
 sudo /etc/init.d/dbus restart
 sudo service dbus start
@@ -35,11 +40,12 @@ sudo chown -R ${USER}:${USER} $HOME/.config
 
 ## setup --no-sandbox for google-chrome replacement
 sudo cp /opt/google/chrome/google-chrome /opt/google/chrome/google-chrome.ORIG
+
 cat >./google-chrome <<-EOF
 #!/bin/bash
 /opt/google/chrome/google-chrome.ORIG --no-sandbox --disable-gpu $*
 EOF
 sudo cp ./google-chrome /opt/google/chrome/google-chrome
 
-sudo apt autoremove -y
+sudo apt-get autoremove -y
 
